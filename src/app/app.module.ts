678b064079dad9c0b612292e9144ca8d4f72cd7c
Kamesh Tagore <kameshtagore@LynxWebs-Mac-mini.local>
2023-02-13 20:58:42 -0800

    added temporary picture and content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { ContactComponent } from './contact/contact.component';
+import { ExtracurricularActivitiesComponent } from './extracurricular-activities/extracurricular-activities.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AchievementsComponent,
+    ContactComponent,
+    ExtracurricularActivitiesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
