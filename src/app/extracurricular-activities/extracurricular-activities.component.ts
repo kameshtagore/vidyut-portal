63ff67735e72ad8b76e4a8132dfe3e89df868834
Kamesh Tagore <kameshtagore@LynxWebs-Mac-mini.local>
2023-02-13 21:03:30 -0800

    added vidyuth's pic

---
+++ src/app/extracurricular-activities/extracurricular-activities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-extracurricular-activities',
+  templateUrl: './extracurricular-activities.component.html',
+  styleUrls: ['./extracurricular-activities.component.css']
+})
+export class ExtracurricularActivitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
